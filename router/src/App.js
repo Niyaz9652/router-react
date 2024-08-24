1aeae260783acaf75a74c157077759b54c6c5510
Darzi Niyaz Ahammad <niyazdarzi@gmail.com>
2024-08-24 08:28:01 +0530

    first commit

---
+++ router/src/App.js
@@ -0,0 +1,13 @@
+import {Routes,Route} from 'react-router-dom'
+import { About } from './Components/About';
+import { Home } from './Components/Home';
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+    </Routes>
+  );
+}
+
+export default App;
